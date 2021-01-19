b3bcca81747f1bbad9f463c348682903e67e17ba
juan pablo perera <juan.perera@bosc.solutions>
2021-01-19 17:06:18 -0600

    Adding styles

--- screens/Login.js
+++ screens/Login.js
@@ -9,28 +9,44 @@ const Login = () => {
     const { login } = useContext(AuthContext);
 
     return (
-        <View style={{ flex: 1, backgroundColor: '#fff' }}>
+        <View style={styles.body}>
             <StatusBar hidden />
             <Image
                 source={{ uri: 'https://images.pexels.com/photos/1231265/pexels-photo-1231265.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' }}
                 style={[StyleSheet.absoluteFillObject, { opacity: .4 }]}
                 blurRadius={70}
             />
-            <TextInput
-                placeholder="Email"
-                value={username}
-                onChangeText={setUsername}
-                style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginTop: 10 }}
-            />
-            <TextInput
-                style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginTop: 10 }}
-                placeholder="Password"
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry
-            />
-            <Button title="Login" onPress={() => login({ username, password })} />
+            <View style={styles.login}>
+
+                <TextInput
+                    placeholder="Email"
+                    autoCapitalize="none"
+                    value={username}
+                    onChangeText={setUsername}
+                    style={{ width: 200, height: 40, borderBottomColor: 'gray', borderBottomWidth: 1, marginTop: 10 }}
+                />
+                <TextInput
+                    style={{ width: 200, height: 40, borderBottomColor: 'gray', borderBottomWidth: 1, marginTop: 10 }}
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                />
+                <Button title="Login" onPress={() => login({ username, password })} />
+            </View>
         </View>
     );
 }
 export default Login;
+const styles = StyleSheet.create({
+    body: {
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#fff'
+    },
+    login: {
+        flex: 1,
+        justifyContent: 'center'
+    }
+});
